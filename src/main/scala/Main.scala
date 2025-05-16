4fe1d3cde9041e9bbe1f39ffbdc6e76ff240f6d7
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-05-16 12:16:12 +0200

    update Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,18 @@
 
-object Main extends App:
-  println(s"PPS-24-papyrus")
+import papyrus.DSL.PapyrusElement.*
+
+@main def run(): Unit =
+  val doc = papyrus(
+    metadata(
+      List()
+    ),
+    content(
+      List(
+        text("body content"),
+        image("image.png", "An image")
+      )
+    )
+  )
+
+  println(doc.render)
 
