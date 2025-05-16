0ad7994a580e5eeb90fe9e703f77ac64ea4cca43
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-05-16 12:16:33 +0200

    test Papyrus element

---
+++ src/test/scala/DSL/PapyrusElementTest.scala
@@ -0,0 +1,30 @@
+package DSL
+
+import org.scalatest.funsuite.AnyFunSuite
+import papyrus.DSL.PapyrusElement
+
+class PapyrusElementTest extends AnyFunSuite {
+
+  import PapyrusElement._
+
+  test("Text element should render correctly") {
+    val t = text("Hello World")
+    assert(t.render == "<p>Hello World</p>")
+  }
+
+  test("Image element should render correctly") {
+    val img = image("image.png", "An image")
+    assert(img.render == "<img src='image.png' alt='An image'/>")
+  }
+
+  test("Metadata should render list of elements inside <head>") {
+    val meta = metadata(List(text("meta info")))
+    assert(meta.render == "<head><p>meta info</p></head>")
+  }
+
+  test("Content should render list of elements inside <body>") {
+    val cont = content(List(text("body content")))
+    assert(cont.render == "<body><p>body content</p></body>")
+  }
+}
+
