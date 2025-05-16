af40a05af7656d99069f5aa859b323848aba3c68
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-05-16 12:16:40 +0200

    Papyrus element

---
+++ src/main/scala/papyrus/DSL/PapyrusElement.scala
@@ -0,0 +1,42 @@
+package papyrus.DSL
+
+import scala.annotation.internal.Body
+
+object PapyrusElement:
+
+  sealed trait PapyrusElement:
+    def render: String
+
+  private case class Text(content: String) extends PapyrusElement:
+    def render: String = s"<p>${content}</p>"
+
+  case class Metadata(content: List[PapyrusElement]) extends PapyrusElement:
+    def render: String = s"<head>${content.map(_.render).mkString("\n")}</head>"
+
+  case class Content(content: List[PapyrusElement]) extends PapyrusElement:
+    def render: String = s"<body>${content.map(_.render).mkString("\n")}</body>"
+
+  private case class Image(src: String, alt: String) extends PapyrusElement:
+    def render: String = s"<img src='${src}' alt='${alt}'/>"
+    
+  private case class PapyrusDocument(meta: Metadata, body: Content) extends PapyrusElement:
+    def render: String = """<html>""" + "\n" + meta.render + "\n" + body.render + "\n" + """</html>"""
+  
+
+  // DSL entry point
+  def papyrus(meta: => Metadata, body: => Content): PapyrusElement =
+    PapyrusDocument(
+      meta, body
+    )
+
+  def metadata(body: => List[PapyrusElement]): Metadata =
+    Metadata(body)
+    
+  def content(body: => List[PapyrusElement]): Content =
+    Content(body)
+
+  def image(src: String, alt: String): PapyrusElement =
+    Image(src, alt)
+    
+  def text(content: String): PapyrusElement =
+    Text(content)
