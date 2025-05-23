cf3abca482f5d16315f3b18f7b664c1a48e2feb5
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-05-23 15:57:09 +0200

    update Metadata and Style
    Co-Authored-By: LucaCantagallo <74787256+LucaCantagallo@users.noreply.github.com>

--- src/main/scala/papyrus/logic/metadata/Metadata.scala
+++ src/main/scala/papyrus/logic/metadata/Metadata.scala
@@ -6,9 +6,13 @@ import papyrus.logic.Style.Style
 object Metadata:
 
   trait Metadata extends Renderer:
-    def style: Style
-    def nameFile: String
-    def author: String
-    
+    def style(style: Style): Unit
+    def nameFile(name: String): Unit
+    def title(title: String): Unit
+    def author(author: String): Unit
+    def charset(charset: String): Unit
+    def styleSheet(link: String): Unit
+    def language(language: String): Unit
+
   object Metadata:
     def apply() = ???
