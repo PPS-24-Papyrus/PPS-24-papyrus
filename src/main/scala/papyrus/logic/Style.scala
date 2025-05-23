841a73555e8ac81a0a40a4234550ae0086a31676
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-05-23 15:57:23 +0200

    Update Style.scala
    Co-Authored-By: LucaCantagallo <74787256+LucaCantagallo@users.noreply.github.com>

--- src/main/scala/papyrus/logic/Style.scala
+++ src/main/scala/papyrus/logic/Style.scala
@@ -2,11 +2,21 @@ package papyrus.logic
 
 import papyrus.logic.Renderer.RendererStyle
 
+import java.awt.Color
+
 object Style:
-   
+
    trait Style extends RendererStyle:
-     def color: String
-     
+     def textColor(color: Color): Unit
+     def backgroundColor(color: Color): Unit
+     def font(font: String): Unit
+     def fontSize(size: Int): Unit
+     def margin(top: Int, right: Int, bottom: Int, left: Int): Unit
+     def margin(margin: Int): Unit
+
+
+
+
    object Style:
      def apply() = ???
-     
+
