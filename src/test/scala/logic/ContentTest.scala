bc677e173c9c1b78558a69df18fed767c4fead27
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-05-26 14:42:46 +0200

    refactory Content, ContentTest, Papyrus

--- src/test/scala/logic/ContentTest.scala
+++ src/test/scala/logic/ContentTest.scala
@@ -1,12 +1,14 @@
 package logic
 
 import org.scalatest.funsuite.AnyFunSuite
-import papyrus.logic.Content.Content
+import papyrus.logic.content.Content
 import papyrus.logic.layerElement.LayerElement
+import papyrus.logic.layerElement.text.Title
+import papyrus.logic.Utility.TypesInline.Level
 
 class ContentTest extends AnyFunSuite:
 
   test("Content should render title correctly") 
-    val content = Content("Test Title")
+    val content = Content(Title("Test Title", 1))
     assert(content.render.contains("<h1>Test Title</h1>"))
   
