7f7a850dd1859567a1fde35aa75d997a9741b967
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-05-26 13:58:40 +0200

    refactory metadata

--- src/main/scala/papyrus/logic/metadata/Metadata.scala
+++ src/main/scala/papyrus/logic/metadata/Metadata.scala
@@ -14,19 +14,17 @@ trait Metadata extends Renderer:
   def language(language: Language): String
   
 object Metadata:
+  def apply(): Metadata = MetadataImpl()
 
-  object Metadata:
-    def apply(): Metadata = MetadataImpl()
+  private class MetadataImpl() extends Metadata:
+    override def style(style: Style): String = ???
+    private def meta(tag: String)(value: String): String = s"""<meta name="$tag" content="$value">"""
+    override def nameFile(name: String): String = ??? //Cos'è già questo?
+    override def title(title: String = "New Papyrus"): String = this.meta("title")(title)
+    override def author(author: String = "Unknown"): String = this.meta("author")(author)
+    override def charset(charset: Charset): String = this.meta("charset")(charset)
+    override def styleSheet(link: StyleSheet): String = s"""<link rel="stylesheet" href="$link">""" //se non erro questo tag non può essere visto da user
+    override def language(language: Language): String = s"""lang="$language"""" //va direttamente nel tag html
 
-    private class MetadataImpl() extends Metadata:
-      override def style(style: Style): String = ???
-      private def meta(tag: String)(value: String): String = s"""<meta name="$tag" content="$value">"""
-      override def nameFile(name: String): String = ??? //Cos'è già questo?
-      override def title(title: String = "New Papyrus"): String = this.meta("title")(title)
-      override def author(author: String = "Unknown"): String = this.meta("author")(author)
-      override def charset(charset: Charset): String = this.meta("charset")(charset)
-      override def styleSheet(link: StyleSheet): String = s"""<link rel="stylesheet" href="$link">""" //se non erro questo tag non può essere visto da user
-      override def language(language: Language): String = s"""lang="$language"""" //va direttamente nel tag html
-
-      override def render: String = ???
-      override def renderStyle: String = ???
+    override def render: String = ???
+    override def renderStyle: String = ???
