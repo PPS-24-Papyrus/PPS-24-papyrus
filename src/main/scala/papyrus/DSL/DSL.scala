e1eede30ccf0c22e95fa559abc7bdd16066dc638
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-05-27 15:26:59 +0200

    Update DSL.scala
    Co-Authored-By: LucaCantagallo <74787256+LucaCantagallo@users.noreply.github.com>

--- src/main/scala/papyrus/DSL/DSL.scala
+++ src/main/scala/papyrus/DSL/DSL.scala
@@ -5,52 +5,60 @@ import papyrus.logic.metadata.Metadata
 import papyrus.logic.content.Content
 import papyrus.logic.layerElement.text.Title
 import papyrus.logic.utility.TypesInline.Level
+import io.github.iltotore.iron.autoRefine
+import papyrus.logic.layerElement.LayerElement
+import scala.collection.mutable.ArrayBufferg
 
 object DSL:
 
   class PapyrusBuilder:
-    private var metadata: Metadata = Metadata()
-    private var content: Content = Content()
-
-    def metadata(init: MetadataBuilder ?=> Unit): Unit =
-      given builder: MetadataBuilder = MetadataBuilder()
-      init
-      metadata = builder.build()
-
-    def content(init: ContentBuilder ?=> Unit): Unit =
-      given builder: ContentBuilder = ContentBuilder()
-      init
-      content = builder.build()
+    var metadata: Metadata = Metadata()
+    var content: Content = Content(Title("Default Title", 1))
 
     def build(): Papyrus = Papyrus(metadata, content)
 
+
   class MetadataBuilder:
-    private var name: String = "untitled"
-    private var title: String = "New Papyrus"
-    private var author: String = "Unknown"
 
-    def nameFile(value: String): Unit = name = value
-    def title(value: String): Unit = title = value
-    def author(value: String): Unit = author = value
-
-    def build(): Metadata = Metadata(name, title, author)
+    def build(): Metadata = Metadata()
 
   class ContentBuilder:
-    private val elements = scala.collection.mutable.ListBuffer.empty[Any]
+    var title: Title = ???
+    val layerElements = ArrayBuffer[LayerElement] 
+    
+    def addLayerElement(element: LayerElement): Unit =
+      layerElements += element
+    
+    def build(): Content = Content(Title("Default Title", 1))
 
-    def title(text: String, level: Level): Unit =
-      elements += Title(text, level)
 
-    def text(value: String): Unit =
-      elements += value
-
-    def build(): Content = Content(elements: _*)
 
   def papyrus(init: PapyrusBuilder ?=> Unit): Papyrus =
     given builder: PapyrusBuilder = PapyrusBuilder()
     init
     builder.build()
 
-    papyrus:
-      metadat:
-      content:
+  def metadata(init: MetadataBuilder ?=> Unit)(using PapyrusBuilder: PapyrusBuilder): Unit =
+    given builder: MetadataBuilder = MetadataBuilder()
+    init
+    PapyrusBuilder.metadata = builder.build()
+
+  def content(init: ContentBuilder ?=> Unit)(using PapyrusBuilder: PapyrusBuilder): Unit =
+    given builder: ContentBuilder = ContentBuilder()
+    init
+    PapyrusBuilder.content = builder.build()
+    
+  def title(text: String, level: Level = 1)(using contentBuilder: ContentBuilder): Unit =
+    contentBuilder.build().title = Title(text, level)
+    
+  def aCasoMetadata(a: String)(using metadataBuilder: MetadataBuilder): Unit =
+    println(a)
+
+  def aCasoContent(a: String)(using metadataBuilder: MetadataBuilder): Unit =
+    println(a)
+
+  papyrus:
+    metadata:
+      aCasoMetadata("aa")
+    content:
+      aCasoContent("aa")
