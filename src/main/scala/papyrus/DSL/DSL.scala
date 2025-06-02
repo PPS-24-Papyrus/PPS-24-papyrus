e7d00e8300a010ba315b44d3338ac016515e0841
luca.cantagallo <luca.cantagallo99@gmail.com>
2025-06-02 14:40:28 +0200

    Enumeration

--- src/main/scala/papyrus/DSL/DSL.scala
+++ src/main/scala/papyrus/DSL/DSL.scala
@@ -33,8 +33,8 @@ object DSL:
     val baseTitle = init.str
     val numberedTitle = ctx match
       case _: ContentBuilder => baseTitle
-      case _: SectionBuilder => SectionCounter.nextSection() + ": " + baseTitle
-      case _: SubSectionBuilder => SectionCounter.nextSubsection() + ": " + baseTitle
+      case _: SectionBuilder => SectionCounter.nextSection() + " " + baseTitle
+      case _: SubSectionBuilder => SectionCounter.nextSubsection() + " " + baseTitle
 
     builder.title = numberedTitle
 
