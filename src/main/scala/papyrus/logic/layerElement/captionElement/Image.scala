d4cc1e22173e4b70b0dff238150d8ebd70f5cb30
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-06-02 12:22:27 +0200

    create Image

---
+++ src/main/scala/papyrus/logic/layerElement/captionElement/Image.scala
@@ -0,0 +1,39 @@
+package papyrus.logic.layerElement.captionElement
+
+import papyrus.logic.utility.IdGenerator
+import papyrus.logic.utility.TypesInline.Float
+import papyrus.logic.utility.TypesInline.Width
+
+trait Image extends CaptionElement:
+  def src: String
+  def alt: String
+  def width: Option[Width]
+  def alignment: Option[Float]
+
+object Image:
+  def apply(src: String, alt: String, caption: Option[String], width: Option[Width],  alignment: Option[Float]): Image = new ImageImpl(src, alt, caption, width, alignment)
+
+  private class ImageImpl(override val src: String,
+                          override val alt: String,
+                          override val caption: Option[String],
+                          override val width: Option[Width],
+                          override val alignment: Option[Float]) extends Image:
+    private val idImage: String = "cls-" + IdGenerator.nextId()
+    private val idFigure: String = "cls-" + IdGenerator.nextId()
+
+    override def render: String =
+      val captionString: String = if this.caption.isEmpty then "" else s"<figcaption>${caption.get}</figcaption>"
+      s"""<figure class="$idFigure">
+        <img class="$idImage" src="$src" alt="$alt">
+        $captionString
+      </figure>"""
+
+    override def renderStyle: String =
+      val widthStyle = if width.isEmpty then "" else s"width: ${width.get};\nheight: auto;"
+      val alignmentStyle = if alignment.isEmpty then "display: block; margin: 0 auto;" else s"float: ${alignment.get};"
+      if width.isEmpty && alignment.isEmpty then ""
+      else
+        s""".$idImage {\n  $widthStyle\n}
+           |
+           |.$idFigure {\n  text-align: center;\n  $alignmentStyle\n}
+           |""".stripMargin
