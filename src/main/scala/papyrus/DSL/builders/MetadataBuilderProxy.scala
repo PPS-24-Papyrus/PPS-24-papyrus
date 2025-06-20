19014c534734ad79a2edcbfa8eb3c5720eaab3ab
luca.cantagallo <luca.cantagallo99@gmail.com>
2025-06-20 19:28:45 +0200

    MetadataBuilderProxy documentation

--- src/main/scala/papyrus/DSL/builders/MetadataBuilderProxy.scala
+++ src/main/scala/papyrus/DSL/builders/MetadataBuilderProxy.scala
@@ -1,62 +1,88 @@
 package papyrus.DSL.builders
 
-import papyrus.logic.utility.TypesInline.*
-import io.github.iltotore.iron.autoRefine
 import papyrus.logic.metadata.Metadata
+import papyrus.logic.utility.TypesInline.{Charset, Extension, Language, StyleSheet}
+import papyrus.DSL.DefaultValues
 
-class MetadataBuilderProxy(
-                            get: () => MetadataBuilder,
-                            set: MetadataBuilder => Unit
-                          ) extends MetadataBuilder() {
+enum Field:
+  case NameFile, Extension, SavingPath, Language, Title, Author, Charset, StyleSheet, Style
 
-  override def withNameFile(value: String): MetadataBuilder =
-    val updated = get().withNameFile(value)
-    set(updated)
-    this
+/** Builds document metadata with constrained fields */
+case class MetadataBuilder(
+                            nameFile: String = DefaultValues.nameFile,
+                            extension: Extension = DefaultValues.extension,
+                            savingPath: String = "",
+                            language: Language = DefaultValues.language,
+                            title: String = DefaultValues.title,
+                            author: String = DefaultValues.author,
+                            charset: Charset = DefaultValues.charset,
+                            styleSheet: StyleSheet = DefaultValues.styleSheet,
+                            styleBuilder: MainStyleBuilder = MainStyleBuilder(),
+                            modifiedFields: Set[Field] = Set.empty
+                          ) extends Builder[Metadata]:
 
-  override def withExtension(value: Extension): MetadataBuilder =
-    val updated = get().withExtension(value)
-    set(updated)
-    this
+  private def setOnce[T](field: Field, update: MetadataBuilder => MetadataBuilder)(value: T): MetadataBuilder =
+    if modifiedFields.contains(field) then
+      throw new IllegalStateException(s"$field has already been set")
+    update(this).copy(modifiedFields = modifiedFields + field)
 
-  override def withSavingPath(value: String): MetadataBuilder =
-    val updated = get().withSavingPath(value)
-    set(updated)
-    this
+  def getStyleBuilder: MainStyleBuilder = styleBuilder
 
-  override def withLanguage(value: Language): MetadataBuilder =
-    val updated = get().withLanguage(value)
-    set(updated)
-    this
+  def withNameFile(value: String): MetadataBuilder =
+    setOnce(Field.NameFile, _.copy(nameFile = value))(value)
 
-  override def withTitle(value: String): MetadataBuilder =
-    val updated = get().withTitle(value)
-    set(updated)
-    this
+  def withExtension(value: Extension): MetadataBuilder =
+    setOnce(Field.Extension, _.copy(extension = value))(value)
 
-  override def withAuthor(value: String): MetadataBuilder =
-    val updated = get().withAuthor(value)
-    set(updated)
-    this
+  def withSavingPath(value: String): MetadataBuilder =
+    setOnce(Field.SavingPath, _.copy(savingPath = value))(value)
 
-  override def withCharset(value: Charset): MetadataBuilder =
-    val updated = get().withCharset(value)
-    set(updated)
-    this
+  def withLanguage(value: Language): MetadataBuilder =
+    setOnce(Field.Language, _.copy(language = value))(value)
 
-  override def withStyleSheet(value: StyleSheet): MetadataBuilder =
-    val updated = get().withStyleSheet(value)
-    set(updated)
-    this
+  def withTitle(value: String): MetadataBuilder =
+    setOnce(Field.Title, _.copy(title = value))(value)
 
-  override def withStyle(value: MainStyleBuilder): MetadataBuilder =
-    val updated = get().withStyle(value)
-    set(updated)
-    this
+  def withAuthor(value: String): MetadataBuilder =
+    setOnce(Field.Author, _.copy(author = value))(value)
 
-  override def getStyleBuilder: MainStyleBuilder =
-    get().getStyleBuilder
+  def withCharset(value: Charset): MetadataBuilder =
+    setOnce(Field.Charset, _.copy(charset = value))(value)
+
+  def withStyleSheet(value: StyleSheet): MetadataBuilder =
+    setOnce(Field.StyleSheet, _.copy(styleSheet = value))(value)
+
+  def withStyle(value: MainStyleBuilder): MetadataBuilder =
+    this.copy(styleBuilder = value)
 
   override def build: Metadata =
-    get().build
-}
+    Metadata(nameFile, extension, savingPath, styleBuilder.build, language, title, author, charset, styleSheet)
+
+object MetadataBuilder:
+  extension (mb: MetadataBuilder)
+    /** Sets filename */
+    def nameFile(value: String): MetadataBuilder = mb.withNameFile(value)
+
+    /** Sets file extension ("html" or "pdf") */
+    def extension(value: Extension): MetadataBuilder = mb.withExtension(value)
+
+    /** Sets absolute saving path */
+    def savingPath(value: String): MetadataBuilder = mb.withSavingPath(value)
+
+    /** Sets language ("en", "it", "fr", "de", etc.) */
+    def language(value: Language): MetadataBuilder = mb.withLanguage(value)
+
+    /** Sets document title */
+    def title(value: String): MetadataBuilder = mb.withTitle(value)
+
+    /** Sets author name */
+    def author(value: String): MetadataBuilder = mb.withAuthor(value)
+
+    /** Sets charset ("utf-8", "iso-8859-1", "windows-1252") */
+    def charset(value: Charset): MetadataBuilder = mb.withCharset(value)
+
+    /** Sets stylesheet path (String ending with ".css"). Papyrus creates a default stylesheet */
+    def styleSheet(value: StyleSheet): MetadataBuilder = mb.withStyleSheet(value)
+
+    /** Sets style parameters with another builder */
+    def style(value: MainStyleBuilder): MetadataBuilder = mb.withStyle(value)
