a28deb577bbfda0f53b2b1c571957af5a37cda87
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-06-23 11:28:12 +0200

    update report

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,19 @@
+export default {
+    title: 'Papyrus',
+    description: 'Relazione tecnica del progetto Papyrus',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' }
+            // puoi aggiungere altre pagine/link qui
+        ],
+        sidebar: [
+            {
+                text: 'Indice',
+                items: [
+                    { text: 'Introduzione', link: '/' }
+                    // aggiungi altri capitoli se vuoi
+                ]
+            }
+        ]
+    }
+}
